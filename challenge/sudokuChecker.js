d2667bd33220f8eeb01f1ff5b167a3de4b92a334
Calvin Lee <calsmlee@uw.edu>
2016-11-05 14:09:10 -0700

    Sudoku checker complete

--- challenge/sudokuChecker.js
+++ challenge/sudokuChecker.js
@@ -3,7 +3,7 @@
 // Description:
 // Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' if the board is invalid.
 
-// A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9. 
+// A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
 // (!!!) NOTICE: Newline characters in the inputs have been replaced with <br /> tags to make the value easier to read. In other words, when you see a break, it's actually a
 
@@ -22,6 +22,41 @@
             // 157638429) => 'solved'
 
 // Code:
-function sudokuCheck (boardStr) {
+const sudokuCheck = boardStr => {
+  let rows = boardStr.split('\n'), valid = true, cols = [], blocks = [];
+  rows.forEach(() => {
+    cols.push([]), blocks.push([]);
+  })
 
+  const checkGroup = (rowArr, nums) => {
+    for(let num of rowArr) {
+      if(nums.includes(num))
+        return false;
+      nums.push(num);
+    }
+    return true;
+  }
+
+  for(var j = 0; j < rows.length; j++) {
+    let currBlock = 0 + 3 *parseInt(j/3);
+    rows[j].split('').forEach((element, index) => {
+      cols[index].push(element);
+      if(index%3 === 0 && index>0)
+        currBlock++;
+      blocks[currBlock].push(element);
+    })
+    valid = checkGroup(rows[j].split(''), []);
+    if(!valid) return 'invalid';
+  }
+
+  for(let col of cols) {
+    valid = checkGroup(col, []);
+    if(!valid) return 'invalid';
+  }
+
+  for(let block of blocks) {
+    valid = checkGroup(block, []);
+    if(!valid) return 'invalid';
+  }
+  return 'solved';
 }
