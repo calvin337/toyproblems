3d6d2b8f41d8e71077cf55b4e2ac932923c4c637
Calvin Lee <calsmlee@uw.edu>
2016-12-01 11:24:52 -0800

    QueueStack complete

--- challenge/queueStack.js
+++ challenge/queueStack.js
@@ -8,16 +8,16 @@
 var Stack = function() {
   var storage = [];
 
-  this.push = function(){
-
+  this.push = function(val){
+    storage.push(val);
   };
 
   this.pop = function(){
-
+    return storage.pop();
   };
 
   this.size = function(){
-
+    return storage.length;
   };
 };
 
@@ -26,15 +26,18 @@ var Queue = function() {
   var inbox = new Stack();
   var outbox = new Stack();
 
-  this.enqueue = function(){
-
+  this.enqueue = function(val){
+    inbox.push(val);
   };
 
   this.dequeue = function(){
- 
+    while(inbox.size() !== 0) outbox.push(inbox.pop());
+    let remove = outbox.pop();
+    while(outbox.size() !== 0) inbox.push(outbox.pop());
+    return remove;
   };
 
   this.size = function(){
-
+    return inbox.size();
   };
 };
